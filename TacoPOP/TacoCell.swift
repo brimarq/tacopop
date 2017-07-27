3d7d2ab0342f6a9dfbb88a6274291e0dbbd49a3b
Brian Marquis <rbmarquis.dev@gmail.com>
2017-07-26 19:59:19 -0500

    Setup collection view and collection view cell

---
+++ TacoPOP/TacoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TacoCell.swift
+//  TacoPOP
+//
+//  Created by Brian Marquis on 7/26/17.
+//  Copyright © 2017 brimarq. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    
+    var taco: Taco!
+    
+    func configureCell(taco: Taco) {
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinID.rawValue)
+        tacoLabel.text = taco.productName
+    }
+
+}
